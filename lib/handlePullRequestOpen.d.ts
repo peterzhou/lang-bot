ab7dfe4b3eb5ee3cf1e1db2a1443383b29c61e73
Peter Zhou <peter.zhou@yale.edu>
2019-09-01 21:40:09 -0700

    remove lib

--- lib/handlePullRequestOpen.d.ts
+++
@@ -1,3 +0,0 @@
-import { Context } from "probot";
-import Webhooks = require("@octokit/webhooks");
-export declare function handlePullRequestOpen(context: Context<Webhooks.WebhookPayloadPullRequest>): Promise<void>;
